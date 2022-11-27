61dbf1587c88385a558f648f26dd570c1e574506
Richard <nemogz@student.ubc.ca>
2022-11-26 21:51:38 -0800

    added the counter class and created first template

---
+++ app/src/main/java/com/nemogz/mantracounter/Counter.java
@@ -0,0 +1,100 @@
+package com.nemogz.mantracounter;
+
+import android.content.Context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Counter {
+
+    private static final int DaBeiLimit = 27;
+    private static final int BoRuoLimit = 49;
+    private static final int XiaoZhaiLimit = 84;
+    private static final int QiFoLimit = 87;
+    private static final String dabei = MainActivity.getAppResources().getString(R.string.dabei);
+    private static final String boruo = MainActivity.getAppResources().getString(R.string.boruo);
+    private static final String xiaozai = MainActivity.getAppResources().getString(R.string.xiaozai);
+    private static final String qifo = MainActivity.getAppResources().getString(R.string.qifo);
+    private static Map<String, Integer> littleHouse = new HashMap<>();
+
+
+    private String name;
+    private Integer count;
+
+    public Counter(String name, Integer count) {
+        this.name = name;
+        this.count = count;
+        littleHouse = new HashMap<>();
+    }
+
+    /**
+     * Initializes the LittleHouse map
+     */
+    public static void createLittleHouse(){
+        littleHouse.put(dabei, 0);
+        littleHouse.put(boruo, 0);
+        littleHouse.put(xiaozai, 0);
+        littleHouse.put(qifo, 0);
+        littleHouse.put(MainActivity.getAppResources().getString(R.string.xiaofangzi), 0);
+    }
+
+    /**
+     * increments the count value by 1
+     * checks if the mantra count amount has reached the threshold if yes updates the littleHouse count
+     */
+    public void increment(){
+        count++;
+
+        //check if each mantra count is satisfied
+        if((name.equals(dabei)) && (count >= DaBeiLimit)){
+            littleHouse.put(dabei, littleHouse.get(dabei + 1));
+            updateLittleHouse();
+            count = count - DaBeiLimit;
+        }
+        else if(name.equals(boruo) && count >= BoRuoLimit){
+            littleHouse.put(boruo, littleHouse.get(boruo + 1));
+            updateLittleHouse();
+            count = count - BoRuoLimit;
+        }
+        else if(name.equals(xiaozai) && count >= XiaoZhaiLimit){
+            littleHouse.put(xiaozai, littleHouse.get(xiaozai + 1));
+            updateLittleHouse();
+            count = count - XiaoZhaiLimit;
+        }
+        else if(name.equals(qifo) && count >= QiFoLimit){
+            littleHouse.put(qifo, littleHouse.get(qifo + 1));
+            updateLittleHouse();
+            count = count - QiFoLimit;
+        }
+
+    }
+
+    /**
+     * Set the count amount to the newCount
+     * @param newCount the new count amount to set
+     */
+    public void setCount(int newCount){
+        count = newCount;
+    }
+
+    /**
+     * Decrements the count value by 1
+     */
+    public void decrement(){
+        count--;
+    }
+
+    /**
+     * Update the littleHouse count if the difference mantras reached the recitation threshold
+     * Also decrements the mantra threshold amount
+     */
+    private static void updateLittleHouse(){
+        if((littleHouse.get(dabei) == 1) && littleHouse.get(boruo) == 1 && littleHouse.get(xiaozai) == 1 && littleHouse.get(qifo) == 1){
+            littleHouse.put(MainActivity.getAppResources().getString(R.string.xiaofangzi), littleHouse.get(MainActivity.getAppResources().getString(R.string.xiaofangzi) + 1));
+            littleHouse.put(dabei, littleHouse.get(dabei) - 1);
+            littleHouse.put(boruo, littleHouse.get(boruo) - 1);
+            littleHouse.put(xiaozai, littleHouse.get(xiaozai) - 1);
+            littleHouse.put(qifo, littleHouse.get(qifo) - 1);
+        }
+    }
+}
--- app/src/main/java/com/nemogz/mantracounter/MainActivity.java
+++ app/src/main/java/com/nemogz/mantracounter/MainActivity.java
@@ -2,21 +2,32 @@ package com.nemogz.mantracounter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    //to access the strings in classes
+    private static Resources resources;
     private Button buttonMain;
     private int totalMainCount = 0;
+
+    private Boolean addMode = true;
 //    private Vibrator vibrator;
 //    private boolean vibrate = false;
+    private List<Counter> counters = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        resources = getResources();
         setContentView(R.layout.activity_main);
+        createEssentailCounters();
 
 //        if(vibrator.hasVibrator()){
 //            vibrate = true;
@@ -48,5 +59,16 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    public static Resources getAppResources() {
+        return resources;
+    }
+
+    public void createEssentailCounters(){
+        counters.add(new Counter(getString(R.string.dabei), 0));
+        counters.add(new Counter(getString(R.string.boruo), 0));
+        counters.add(new Counter(getString(R.string.xiaozai), 0));
+        counters.add(new Counter(getString(R.string.qifo), 0));
     }
 }
