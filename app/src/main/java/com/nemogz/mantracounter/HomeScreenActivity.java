5403a5d4a8ed7ebe5aff9131ec1a48f79029ba1b
Richard <nemogz@student.ubc.ca>
2022-12-23 15:40:15 -0800

    stuff

--- app/src/main/java/com/nemogz/mantracounter/HomeScreenActivity.java
+++ app/src/main/java/com/nemogz/mantracounter/HomeScreenActivity.java
@@ -1,27 +1,33 @@
 package com.nemogz.mantracounter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import com.nemogz.mantracounter.R;
 import com.nemogz.mantracounter.counterStuff.Counter;
 import com.nemogz.mantracounter.counterStuff.LittleHouse;
 import com.nemogz.mantracounter.counterStuff.MasterCounter;
 import com.nemogz.mantracounter.dataStorage.MasterCounterDatabase;
 import com.nemogz.mantracounter.homescreen.CounterMainRecViewAdapter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
-    private RecyclerView homeRecView;
+    private RecyclerView mantraCountersListView;
+    private CardView littleHouseItemView;
+    private TextView littleHouseNameItem;
+    private TextView littleHouseCountItem;
     public MasterCounterDatabase db;
     private MasterCounter masterCounter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,15 +37,23 @@ public class HomeScreenActivity extends AppCompatActivity {
         if (!loadDataFromDatabase()) {
             createEssentailCounters();
         }
-
-        homeRecView = findViewById(R.id.HomeScreenRecView);
+        instantiateViews();
+        setBasicCounterView();
 
         CounterMainRecViewAdapter counterAdapter = new CounterMainRecViewAdapter(this);
         counterAdapter.setCounters(masterCounter.getCounters());
 
-        homeRecView.setAdapter(counterAdapter);
+        mantraCountersListView.setAdapter(counterAdapter);
         //means the layout is vertical
-        homeRecView.setLayoutManager(new GridLayoutManager(this, 2));
+        mantraCountersListView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        littleHouseItemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent littleHouseScreenIntent = new Intent(getApplicationContext(), LittleHouseItemActivity.class);
+                getApplicationContext().startActivity(littleHouseScreenIntent);
+            }
+        });
     }
 
     public void createEssentailCounters(){
@@ -47,8 +61,24 @@ public class HomeScreenActivity extends AppCompatActivity {
         masterCounter.createBasicCounters();
     }
 
+    private void instantiateViews() {
+        mantraCountersListView = findViewById(R.id.HomeScreenRecView);
+        littleHouseItemView = findViewById(R.id.littleHouseItem);
+        littleHouseCountItem = findViewById(R.id.littleHouseCountItem);
+        littleHouseNameItem = findViewById(R.id.littleHouseNameItem);
+    }
+
+    private void setBasicCounterView() {
+        littleHouseNameItem.setText(R.string.xiaofangzi);
+        littleHouseCountItem.setText(masterCounter.getLittleHouse().getLittleHouseCount().toString());
+    }
+
     private void createDataBase(Context context) {
         db = MasterCounterDatabase.getINSTANCE(getApplicationContext());
+    }
+
+    private void updateLittleHouseCounts() {
+
     }
 
     /**
---
+++ app/src/main/java/com/nemogz/mantracounter/LittleHouseItemActivity.java
@@ -0,0 +1,17 @@
+package com.nemogz.mantracounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class LittleHouseItemActivity extends AppCompatActivity {
+
+    TextView test;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_littlehouse_item_screen);
+        test = findViewById(R.id.littleHouseViewTest);
+    }
+}
