02545cf94ab2dbc38a1d65f15931e1f48758c168
Richard <nemogz@student.ubc.ca>
2022-12-24 13:24:00 -0800

    donno why

--- app/src/main/java/com/nemogz/mantracounter/settings/SettingsDataClass.java
+++ app/src/main/java/com/nemogz/mantracounter/settings/SettingsDataClass.java
@@ -10,7 +10,7 @@ import org.jetbrains.annotations.NotNull;
 public class SettingsDataClass {
 
     @ColumnInfo
-    private boolean homeSelectTrash;
+    private gitboolean homeSelectTrash;
 
     @PrimaryKey
     @NotNull
